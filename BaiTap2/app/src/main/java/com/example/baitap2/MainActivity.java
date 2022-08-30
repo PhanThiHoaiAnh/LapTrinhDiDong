215edada70433956eb5004a2a1bb415d92eaeeb8
PhanThiHoaiAnh <anhphan0327@gmail.com>
2022-08-30 22:15:00 +0700

    hoan thanh bai tap 2

--- BaiTap2/app/src/main/java/com/example/baitap2/MainActivity.java
+++ BaiTap2/app/src/main/java/com/example/baitap2/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.baitap2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnDangNhap;
+    Object MainActivity2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnDangNhap = findViewById(R.id.btnDN);
+        btnDangNhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMainActivity2();
+            }
+        });
     }
+
+    public void openMainActivity2() {
+        Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+        startActivity(intent);
+    }
+
 }
